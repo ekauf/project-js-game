dae9ceea2ed46f186e11a7b9ae5f525ce56742ea
Edgars <Edgars.Kaufmanis@nology.io>
2022-11-30 16:27:27 +0000

    added input letter, set up input key functions

---
+++ script.js
@@ -0,0 +1,33 @@
+const letterKeys = document.querySelectorAll("button");
+const gameboardRow = document.querySelectorAll(".gameboard__row");
+
+let row = 0;
+let column = 0;
+
+// const inputLetter = letterKeys.forEach((letter) => {
+//   gameboardRow[0].wordBox[0].innerHTML += letter.innerHTML;
+// });
+
+const inputLetter = (letter) => {
+  const currentRow = gameboardRow[row];
+  const currentBox = currentRow.children[column];
+  currentBox.innerHTML += letter;
+  column++;
+  console.log(currentBox);
+};
+
+const inputKey = (event) => {
+  const key = event.target.innerHTML;
+  if (key === "ENTER") {
+    // when click enter, go to next row
+    row++;
+  } else if (key === "&larr;") {
+    // backspace
+  } else {
+    inputLetter(key);
+  }
+};
+
+letterKeys.forEach((element) => {
+  element.addEventListener("click", inputKey);
+});
