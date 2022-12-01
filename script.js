2805f3e6b7389c8e02c49032012941d60e7c1aa1
Edgars <Edgars.Kaufmanis@nology.io>
2022-12-01 15:01:51 +0000

    added checkWord functionality: check against indexes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const letterKeys = document.querySelectorAll("button");
 const gameboardRow = document.querySelectorAll(".gameboard__row");
 
+const wordAnswer = "SOUND";
+
 let row = 0;
 let column = 0;
 
@@ -8,17 +10,51 @@ const inputLetter = (letter) => {
   if (column < 5) {
     const currentRow = gameboardRow[row];
     const currentBox = currentRow.children[column];
-    currentBox.innerHTML += letter;
+    currentBox.innerText += letter;
     column++;
     console.log(currentBox);
   }
 };
 
+const checkWord = () => {
+  const currentRow = gameboardRow[row];
+  const currentRowLetters = currentRow.children;
+
+  const currentRowLettersArray = Array.from(currentRowLetters);
+  const letterArray = currentRowLettersArray.map((div) => {
+    return div.innerText;
+  });
+  console.log(letterArray);
+
+  letterArray.forEach((letter) => {
+    const indexOfWordAnswer = wordAnswer.indexOf(letter);
+    console.log(indexOfWordAnswer);
+  });
+
+  //   if (currentRowLetters === wordAnswer.indexOf(wordAnswer)) {
+  //     alert("Correct");
+  //   } else {
+  //     alert("Incorrect");
+  //   }
+  //   currentRowLetters.forEach((letter) => {
+  //     const indexOfWordAnswer = wordAnswer.indexOf(letter);
+  //     console.log(indexOfWordAnswer);
+  //   });
+};
+
+const clickEnter = () => {
+  if (column < 5) {
+    alert("Not enough letters");
+  } else {
+    checkWord();
+  }
+};
+
 const inputKey = (event) => {
-  const key = event.target.innerHTML;
+  const key = event.target.innerText;
   if (key === "ENTER") {
-    // when click enter, go to next row
-    row++;
+    clickEnter();
+    // row++;
   } else if (key === "&larr;") {
     // backspace
   } else {
